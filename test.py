3a912af308983262091898adbd021cfe266c6bca
Kevin <keving.personal@gmail.com>
2021-09-17 11:05:27 -0700

    Reset on done

--- test.py
+++ test.py
@@ -31,4 +31,7 @@ model = DQN.load("deepq_1x1")
 obs = env.reset()
 while True:
     action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
+    obs, rewards, done, info = env.step(action)
+
+    if done == True:
+        obs = env.reset()
